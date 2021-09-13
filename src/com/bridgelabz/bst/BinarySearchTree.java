1c5aac68cf69d69c4ed9e0bc4bb32b24da93f54a
niharika-1999 <niharika.rao@ymedialabs.com>
2021-09-13 18:19:00 +0530

    Added:Create BST

---
+++ src/com/bridgelabz/bst/BinarySearchTree.java
@@ -0,0 +1,64 @@
+package com.bridgelabz.bst;
+
+/*Binary Search Tree Program in which constructor
+ * used to create new node 
+ */
+
+class Node
+{
+	int data;
+	Node left_node;
+	Node right_node;
+	public Node(int item)
+	{
+		data=item;
+		left_node=right_node=null;
+				
+	}
+}
+
+public class BinarySearchTree {
+	Node root;
+	BinarySearchTree()
+	{
+		root=null;
+	}
+	void add(int key)
+	{
+		root=add(root,key);
+	}
+	
+	Node add(Node root,int key) //add method used to check if tree is null should return new node or recur down
+	{
+		if(root==null)
+		{
+			root=new Node(key);
+			return root;
+		}
+		if(key<root.data)
+		{
+			root.left_node=add(root.left_node,key);
+		}
+		else if(key>root.data)
+		{
+			root.right_node=add(root.right_node,key);
+		}
+		return root;
+	}
+	
+	void inorder() //To display LinkedList
+	{
+		traverse(root);
+	}
+	
+	void traverse(Node root) //inorder traversal of Binary Search Tree
+	{
+		if(root!=null)
+		{
+			traverse(root.left_node);
+			System.out.println(root.data);
+			traverse(root.right_node);
+		}
+	}
+
+}
---
+++ src/com/bridgelabz/bst/BinaryTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.bst;
+
+public class BinaryTest {
+	
+	public static void main(String[] args) //MainMethod
+	{
+		BinarySearchTree node=new BinarySearchTree();
+		node.add(56);
+		node.add(70);
+		node.add(30);
+		node.inorder();
+	}
+
+}
